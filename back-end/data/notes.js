44bf08628e4c3156d1a26377d5931d29a13104fb
srinivas7661 <srinivas@leewayhertz.com>
2024-02-25 10:37:25 +0530

    initial commit

---
+++ back-end/data/notes.js
@@ -0,0 +1,39 @@
+const notes = [
+  {
+    id: "1",
+    title: "Hi how are you",
+    content: "It's good to see you",
+    category: "enquiry",
+  },
+  {
+    id: "2",
+    title: "Hi how are you",
+    content: "It's good to see you",
+    category: "enquiry",
+  },
+  {
+    id: "2",
+    title: "Hi how are you",
+    content: "It's good to see you",
+    category: "enquiry",
+  },
+  {
+    id: "3",
+    title: "Hi how are you",
+    content: "It's good to see you",
+    category: "enquiry",
+  },
+  {
+    id: "4",
+    title: "Hi how are you",
+    content: "It's good to see you",
+    category: "enquiry",
+  },
+  {
+    id: "5",
+    title: "Hi how are you",
+    content: "It's good to see you",
+    category: "enquiry",
+  },
+];
+module.exports = notes;
